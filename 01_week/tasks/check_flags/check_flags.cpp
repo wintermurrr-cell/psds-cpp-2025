246a9710dac22621430c469b49454f2008384cfc
wintermurrr-cell <wintermurrr@gmail.com>
2025-11-28 22:31:49 +0500

    add (solution): add <task_check_flags> task

--- 01_week/tasks/check_flags/check_flags.cpp
+++ 01_week/tasks/check_flags/check_flags.cpp
@@ -1,6 +1,5 @@
-#include <cstdint>
-#include <stdexcept>
-
+#include <cstdint>   // Подключаем библиотеку для работы с целыми числами фиксированного размера
+#include <iostream>  // Для вывода данных
 
 enum class CheckFlags : uint8_t {
     NONE = 0,
@@ -13,6 +12,38 @@ enum class CheckFlags : uint8_t {
     ALL = TIME | DATE | USER | CERT | KEYS | DEST
 };
 
+// Если бит стоит, то выводим текст stringIsTrue.
+// Если не стоит - выводим пустую строку
+std::string enumToString(uint8_t flagsInt, CheckFlags checkFlag, std::string stringIsTrue) {
+
+  // Проверяем на то, выставлен ли бит
+    if (flagsInt & static_cast<uint8_t>(checkFlag)) {
+      return stringIsTrue + ',';
+    }
+    return {};
+}
+
+// Функция для вывода в консоль проверок, которые соответствуют флагам переданного енума
 void PrintCheckFlags(CheckFlags flags) {
-    throw std::runtime_error{"Not implemented"};
+
+    uint8_t flagsInt = static_cast<uint8_t>(flags);
+
+// Проверка выхода за пределы енума
+    if (flagsInt > static_cast<uint8_t>(CheckFlags::ALL)) {
+        return;
+    }
+
+    std::string res; // Строка для текстовых эквивалентов
+    res += enumToString(flagsInt, CheckFlags::TIME, "TIME");
+    res += enumToString(flagsInt, CheckFlags::DATE, "DATE");
+    res += enumToString(flagsInt, CheckFlags::USER, "USER");
+    res += enumToString(flagsInt, CheckFlags::CERT, "CERT");
+    res += enumToString(flagsInt, CheckFlags::KEYS, "KEYS");
+    res += enumToString(flagsInt, CheckFlags::DEST, "DEST");
+
+// Если строка не пустая, то последний символ точно запятая - убираем
+    if (!res.empty())
+        res.pop_back();
+
+     std::cout << "[" << res << "]";
 }
