07abb92b7c48cdf3377156613074b56e0b867ae8
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-09 19:32:55 +0500

    add (solution): add <func_array> task

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,22 @@
-#include <stdexcept>
+#include <stdexcept> // Остался для определения size_t
 
+// Используем using чтобы подставить как параметр функции
+using FuncType = double (**)(double, double);
 
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(double a, double b, FuncType ops, size_t opsSize) {
+
+    // Создаем переменную, куда будет складываться результат
+    double res = 0.0;
+
+    // Перебираем все операции
+    for (size_t i = 0; i < opsSize; i++) {
+        // Проверяем на пустой указатель операции
+        if (ops[i] == nullptr)
+            continue; // пропускаем цикл, если пустой
+
+        // Производим запуск операции и складываем результат
+        res += ops[i](a, b);
+    }
+
+    return res;
 }
