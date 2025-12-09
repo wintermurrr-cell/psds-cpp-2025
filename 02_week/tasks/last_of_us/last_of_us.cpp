6713a820278f6a8bc41a37ecc4328807560adcff
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-09 19:38:50 +0500

    add (solution): add <last_of_us> task

--- 02_week/tasks/last_of_us/last_of_us.cpp
+++ 02_week/tasks/last_of_us/last_of_us.cpp
@@ -1,6 +1,34 @@
-#include <stdexcept>
+using Predicate = bool (*)(int); // Используем using чтобы подставить как параметр функции
 
+const int* FindLastElement(const int* begin, const int* end, Predicate predicate) {
 
-/* return_type */ FindLastElement(/* ptr_type */ begin, /* ptr_type */ end, /* func_type */ predicate) {
-    throw std::runtime_error{"Not implemented"};
+    // Проверяем на корректно переданные указатели
+    if (begin == nullptr
+        || end == nullptr
+        || begin > end)
+        return end;
+
+    // Создаем указатель c результатом функции
+    int* res = nullptr;
+
+    // Отменяем константность, чтобы иметь возможность итерироваться по указателю
+    auto noConstBegin = const_cast<int*>(begin);
+
+    // Проходимся поэлементно по массиву чисел
+    while (noConstBegin != end) {
+
+        // Получаем по текущему указателю значение и передаем в функцию для проверки
+        if (predicate(*noConstBegin))
+            res = noConstBegin; // Записываем значение указателя, если проверка была пройдена
+
+        // Перемещаемся по массиву чисел дальше
+        noConstBegin++;
+    }
+
+    // Если ни одно число не удовлетворяло переданному условию,
+    //  возвращаем указатель конца массива
+    if (res == nullptr)
+        return end;
+
+    return res;
 }
