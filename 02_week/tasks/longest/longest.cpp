05e35731a6a37252625b54baa7d9271cffc22fdd
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-09 19:43:38 +0500

    add (solution): add <longest> task

--- 02_week/tasks/longest/longest.cpp
+++ 02_week/tasks/longest/longest.cpp
@@ -1,6 +1,53 @@
-#include <stdexcept>
+#include <stdexcept> // Остался для определения size_t
 
+char* FindLongestSubsequence(const char* begin, const char* end, size_t& count) {
 
-/* return_type */ FindLongestSubsequence(/* ptr_type */ begin, /* ptr_type */ end, /* type */ count) {
-    throw std::runtime_error{"Not implemented"};
+    // Проверяем на действительные указатели
+    if (begin == nullptr || end == nullptr)
+        return nullptr;
+
+    // Указатель на начало массива не должен быть больше указателя на конец массива
+    if (end < begin)
+        return nullptr;
+
+    // Указатель на начало самой большой последовательности
+    char* LongestStartChar = nullptr;
+    count = 0; // Зануляем счётчик
+
+    // Сбрасываем константность с указателя begin 
+    //  и работаем дальше с неконстантной копией указателя (копией же?)
+    char* noConstBegin = const_cast<char*>(begin);
+
+    // Указатель на символ начала последовательности
+    char* currChar = noConstBegin;
+
+    // Счетчик текущей последовательности символов
+    size_t currCount = 0;
+
+    // Цикл для перебора элементов массива
+    while (noConstBegin != end) {
+        char* symb = noConstBegin++; // Оставляем указатель на текущий begin и переходим у следующему символу
+
+        // Если символы в указателях равны, то увеличиваем счётчик последователньости
+        if (*currChar == *symb) {
+            currCount++;
+
+            // Проверяем, не закончилась ли последовательность символов
+            if (noConstBegin != end)
+                continue; // Если не закончилась, то повотряем цикл
+        }
+
+        // Если попали сюда, то непрерывная последовательность прервана
+        //  или достигли конца массива
+
+        // Если текущая последовательность символов больше предыдущей, то заменяем значения
+        if (currCount > count) {
+            count = currCount;
+            LongestStartChar = currChar;
+        }
+        currChar = symb;
+        currCount = 1;
+    }
+
+    return LongestStartChar;
 }
