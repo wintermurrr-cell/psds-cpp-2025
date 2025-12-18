f1064f18f3595da6689b55488d26475bf806e6d0
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-18 22:19:27 +0500

    add (solution): add <find_all> task

--- 03_week/tasks/find_all/find_all.cpp
+++ 03_week/tasks/find_all/find_all.cpp
@@ -1,6 +1,27 @@
-#include <stdexcept>
+#include <vector>
 
+// Используем using чтобы подставить как параметр функции
+using FuncType = bool (*)(int);
 
-/* return_type */ FindAll(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+std::vector<unsigned long> FindAll(const std::vector<int>& v, FuncType func) {
+
+    // Результирующий вектор
+    std::vector<unsigned long> resVec;
+
+    if (func == nullptr)
+        return resVec;
+
+    // Резервируем максимальное возможное место
+    resVec.reserve(v.size());
+
+    // Цикл с проверками
+    for (size_t i = 0; i < v.size(); ++i) {
+        if (func(v[i]))
+            resVec.push_back(i);
+    }
+
+    // Обрезаем лишние ячейки вектора после выделения места
+    resVec.shrink_to_fit();
+
+    return resVec;
 }
