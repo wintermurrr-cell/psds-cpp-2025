e8c9d6fce20d89c9d714bb39bf05447eae4df1bc
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-18 22:02:30 +0500

    add (solution): add <data_stats> task

--- 03_week/tasks/data_stats/data_stats.cpp
+++ 03_week/tasks/data_stats/data_stats.cpp
@@ -1,11 +1,30 @@
-#include <stdexcept>
-
+#include <math.h>
+#include <vector>
 
 struct DataStats {
     double avg = 0.0;
     double sd = 0.0;
 };
 
-/* return_type */ CalculateDataStats(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+DataStats CalculateDataStats(const std::vector<int>& data) {
+
+    DataStats stats;
+
+    // Возвращаем пустую структуру, если нет данных
+    if (data.empty())
+        return stats;
+
+    // Для решения задачи был использован алгоритм Welford'a для одного прохода
+    for (size_t i = 0; i < data.size(); i++) {
+        const double& value = data[i]; // Получение текущего значения элемента из массива
+
+        double delta = value - stats.avg;        // Разница между текущим значением и средним
+        stats.avg += delta / (i + 1);            // Накопление среднего значения
+        stats.sd += delta * (value - stats.avg); // Накопление суммы квадратов отклонений
+    }
+
+    // Вычисляем стандартное отклонение
+    stats.sd = sqrt(stats.sd / data.size());
+
+    return stats;
 }
