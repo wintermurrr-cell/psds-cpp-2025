1e998dd1ff6b786b5bb911f99350819c646eb247
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-18 22:28:27 +0500

    add (solution): add <filter> task

--- 03_week/tasks/filter/filter.cpp
+++ 03_week/tasks/filter/filter.cpp
@@ -1,6 +1,21 @@
-#include <stdexcept>
+#include <vector>
 
+using Pred = bool (*)(int);
 
-/* return_type */ Filter(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+void Filter(std::vector<int>& v, Pred func) {
+    if (func == nullptr)
+        return;
+
+    int rewrite_indx = 0; // Индекс для перезписи устаревшего элемента
+
+    // Проходимся по всему массиву
+    for (auto itr = v.begin(); itr != v.end(); ++itr) {
+        // Если элемент удовлетворяет условию
+        if (func(*itr)) {
+            v[rewrite_indx] = *itr; // Перепимываем элемент на новое значение, которое нужно оставить
+            ++rewrite_indx;         // Увеличиваем счетчик элемента
+        }
+    }
+
+    v.resize(rewrite_indx); // Удаляем оставшийся массив
 }
