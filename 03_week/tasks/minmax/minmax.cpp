ed0bc4d7d56eef85c5f0bf8f2376a8c2057d76bb
wintermurrr-cell <wintermurrr@gmail.com>
2025-12-18 22:16:52 +0500

    add (solution): add <minmax> task

--- 03_week/tasks/minmax/minmax.cpp
+++ 03_week/tasks/minmax/minmax.cpp
@@ -1,6 +1,28 @@
-#include <stdexcept>
+#include <vector>
 
+using Itr = std::vector<int>::const_iterator; // Для константного итератора используем псевдоним Itr
+using PairItr = std::pair<Itr, Itr>;          // Для пары итераторов используем псевдоним PairItr
 
-/* return_type */ MinMax(/* args */) {
-    throw std::runtime_error{"Not implemented"};
+PairItr MinMax(const std::vector<int>& v) {
+
+    // Создаем пару итераторов, указывающую на начало вектора
+    //  а так же, в случае пустого векотра - и на его конец одновременно
+    PairItr p { v.begin(), v.begin() };
+
+    // Проходимся по всему вектору
+    for (auto it = v.begin(); it != v.end(); ++it) {
+
+        // Если значение first (оно же min) больше текущего значения
+        if (*p.first > *it) {
+            p.first = it; // то заменяем итератор на текущее минимальное
+        }
+
+        // Если значение second (оно же max) меньше или равно текущего значения
+        if (*p.second <= *it) {
+            p.second = it; // то заменяем итератор на текущее максимальное
+        }
+    }
+
+    // Возвращаем пару итераторов min и max
+    return p;
 }
